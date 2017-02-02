6803f6358ba6280a5610c5371647f05aafce8a5c
Niranjan Molkeri <nmolkeri@gmail.com>
2017-02-01 22:44:16 -0800

    Some more variables

---
+++ Ternery operatot.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var amICool = true
+
+amICool = !amICool
+
+var feelGoodAboutMyself = amICool ? true : false //feel good if am cool is true or not if am i cool is false
+
+var bankAccountBalance = 100
+
+var cashRegisterMessage = bankAccountBalance >= 50 ? "You have more than 50 balance" : "You are broke bro"
+
