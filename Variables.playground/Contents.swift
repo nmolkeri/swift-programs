9fe3578995479e3532c326869787fd92efc70f68
Niranjan Molkeri <nmolkeri@gmail.com>
2017-02-01 21:32:47 -0800

    Variables

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,47 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var name = "Niranjan"
+
+name = "Molkeri"
+
+var hi = "Hello"
+
+hi = "Hey"
+
+var x = 10;
+
+var y = 90;
+
+var z = x + y;
+
+print(hi);
+
+print(name);
+
+print(z);
+
+//print(z) coomenting stuff
+
+/*
+ .kasdnbfkjdsf
+sljfdgalisuhfsd
+ ,sjhdbflkasjdf
+ Multi line comment
+*/
+
+var greeting = "Hello"
+var person = "Niranjan"
+
+var greetPerson = greeting + " " + person
+
+print(greetPerson);
+
+var myNumebr = 10.5     //This is double
+
+var 😜 = "Wink"         //This is emoji variable
+
+print(😜)
