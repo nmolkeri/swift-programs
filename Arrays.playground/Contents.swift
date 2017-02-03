965b7dcdbcef67f7c79ef2ae4ac0c0419e0d5da4
Niranjan Molkeri <nmolkeri@gmail.com>
2017-02-03 00:04:13 -0800

    Arrays

---
+++ Arrays.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var emp1Sal = 45000.00
+var emp2Sal = 54000.00
+var employee3Sal = 100000.00
+var employee4Sal = 20000.00
+
+var empSal = [45000.0, 54000.00, 10000, 20000]  //initialize array
+
+empSal.count            //count of elements in array
+empSal.append(39000)    //appending new element
+empSal.count            //count of elements in array
+
+
+empSal.remove(at: 1)    //remove element at position 1
+
+empSal
+
+var students:[String] = []
+
+students.append("Niranjan Molkeri")
+students.append("Jon Doe")
+students.append("Jacob")
