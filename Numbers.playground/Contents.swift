34a26e908841980b1759adf556254e424a51f0d2
Niranjan Molkeri <nmolkeri@gmail.com>
2017-02-02 19:37:47 -0800

    Numbers

---
+++ Numbers.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var age = 27
+
+var weight = 180
+
+var milesRan = 5.6592845394856934825    //it double has precision of more that 15 decimal point
+
+//Arithmetic Operators
+
+var area = 15 * 20
+
+var sum = 5 + 6
+
+var div = 12 / 4
+
+var div1 = 12 / 5
+var remainder = 12 % 5                  //gives th remainder for the equation
+
+var randomNumber = 12
+if randomNumber % 2 == 0
+{
+  print("This is an even number")
+}
+else
+{
+  print("This is an odd number")
+}
